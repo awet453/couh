ce150f24da021db3ac3aa7941f168c4711236464
awet453 <43906274+awet453@users.noreply.github.com>
2018-10-06 14:51:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Alpha Codes `,"http://twitch.tv/Alpha Codes")
+client.user.setGame(`Alpha Codes `,"")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
